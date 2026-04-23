3581d79a54e4cde61657dde3452971d19d910f22
Antsa2008 <anton.tiitu@edu.hyria.fi>
2026-04-23 10:22:33 +0300

    aloitettu
    alkuvaihe

--- paivakirja/paivakirja.cs
+++ paivakirja/paivakirja.cs
@@ -1,1 +1,9 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+
+class Entry
+{
+    public int Id { get; set; }
+    public string Text{ get; set; }
+    public DateTime Date { get; set; }
+}
